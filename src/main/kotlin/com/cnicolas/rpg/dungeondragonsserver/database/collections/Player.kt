70e8889def1082a11790989866e4c90013ff67db
Clement Nicolas <clement.nicolas-ext@jcdecaux.com>
2019-04-18 16:06:38 +0200

    wip(database): Use H2 database instead of mongo (remove completely)

--- src/main/kotlin/com/cnicolas/rpg/dungeondragonsserver/database/collections/Player.kt
+++
@@ -1,7 +0,0 @@
-//package com.cnicolas.rpg.dungeondragonsserver.database.collections
-//
-//import org.springframework.data.annotation.Id
-//import org.springframework.data.mongodb.core.mapping.Document
-//
-//@Document(collection = "players")
-//data class Player(var name: String = "", @Id val id: String? = null)
--- src/main/kotlin/com/cnicolas/rpg/dungeondragonsserver/database/repositories/PlayerRepository.kt
+++ src/main/kotlin/com/cnicolas/rpg/dungeondragonsserver/database/repositories/PlayerRepository.kt
@@ -3,8 +3,6 @@ package com.cnicolas.rpg.dungeondragonsserver.database.repositories
 import com.cnicolas.rpg.dungeondragonsserver.database.entities.Player
 import org.springframework.data.repository.CrudRepository
 
-//interface PlayerMongoRepository : MongoRepository<Player, String>
-
 interface PlayerRepository : CrudRepository<Player, Long> {
     override fun findAll(): List<Player> = findAll()
 }
