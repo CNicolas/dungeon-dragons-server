a6c4cfb7b9a6840286f30b7458b134e78dd61360
Clement Nicolas <clement.nicolas-ext@jcdecaux.com>
2019-04-25 08:58:18 +0200

    feat(database): Add Race

--- src/main/kotlin/com/cnicolas/rpg/dungeondragonsserver/database/entities/Player.kt
+++ src/main/kotlin/com/cnicolas/rpg/dungeondragonsserver/database/entities/Player.kt
@@ -1,10 +1,12 @@
 package com.cnicolas.rpg.dungeondragonsserver.database.entities
 
+import com.cnicolas.rpg.dungeondragonsserver.database.enums.Race
 import javax.persistence.*
 
 @Entity
 @Table
 data class Player(
-    var name: String = "",
+    val name: String = "",
+    val race: Race,
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long
 )
---
+++ src/main/kotlin/com/cnicolas/rpg/dungeondragonsserver/database/enums/Race.kt
@@ -0,0 +1,15 @@
+package com.cnicolas.rpg.dungeondragonsserver.database.enums
+
+enum class Race {
+    HUMAN,
+    DWARF,
+    ELF,
+    HALF_ELF,
+    ORC,
+    HALF_ORC,
+    GOLIATH,
+    HALFLING,
+    GNOME,
+    TIEFFELIN,
+    DRAGONBORN
+}
